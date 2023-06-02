3b47eb33902205d7d7f82c93cf1bb44747f8fb62
Ariel Ayala <arielayalamail@gmail.com>
2023-06-01 21:13:02 -0400

    Add seach icon within input

--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './TodoSearch.css'
+import searchIcon from "../../assets/search.svg";
+
 
 function TodoSearch() {
     return (
-        <input placeholder='Ingrese un texto' />
+        <div className="search-container">
+            <input />
+            <div className="search-icon-container">
+                <img className="search-icon" src={searchIcon} alt="search" />
+            </div>
+        </div>
     )
 }
 
